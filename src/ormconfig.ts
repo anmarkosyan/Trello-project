e2c42da20ec306dbdb53ecec8388c8782bc95086
Hayk Margaryan <haiko64@gmail.com>
2021-10-06 22:04:03 +0400

    basic initial typeorm configuration made in ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,2 +1,22 @@
+import { ConnectionOptions } from "typeorm";
+import dotenv from 'dotenv';
+// import {User} from './entities/User';
 
+dotenv.config();
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    host: 'db',
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD ,
+    database: process.env.POSTGRES_DB,
+    entities: [/*User*/],
+    synchronize: true,
+    migrationsRun: true,
+    logging: false,
+    migrations: [/*migrations folder*/]
+}
+
+export default config;
 
