14d6fa1e860dfbb391f75bd73e03a7d060109dad
anmarkosyan <anush.markos@gmail.com>
2021-10-08 16:36:49 +0400

    added app routing structure

---
+++ src/routes/boardRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { BoardController } from '../controllers/boardController';
+const router = Router();
+
+router
+  .route('/')
+  .get(BoardController.getAllBoards)
+  .post(BoardController.createBoard);
+router
+  .route('/:id')
+  .get(BoardController.getBoard)
+  .patch(BoardController.updateBoard)
+  .delete(BoardController.deleteBoard);
+
+export { router as boardRoute };
