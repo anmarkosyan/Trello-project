d0b79848d10b7fa63da01e07d555f725dfa17907
anmarkosyan <anush.markos@gmail.com>
2021-10-08 16:32:23 +0400

    defined start of the application using typeorm, integrated express with basic route middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
+import app from './app';
 import config from './config';
-
-console.log(config);
-
+import ormconfig from './ormconfig';
 
 
-
-
-
-
-
-
-
-
+//ormconfig file by Hayk
+createConnection(ormconfig)
+  .then(async connection => {
+    console.log('DB connection...');
+    app.listen(config.app.port, () => {
+      console.log(`Starting listen server on port ${config.app.port}...`);
+    });
+  })
+  .catch(e => {
+    console.error('💥 ERROR: Database connection failed!!', e);
+    throw e;
+  });
