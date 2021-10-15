a250ba76fb81ea5d4a3743de5d4985f97d49dd2f
anmarkosyan <anush.markos@gmail.com>
2021-10-15 12:59:36 +0400

    did some code refactoring

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -1,30 +1,87 @@
 import { Request, Response } from 'express';
 import { getManager } from 'typeorm';
 import { BoardRepository } from '../services/board';
+import { Board } from '../entities/Board';
+
+enum Status {
+  Success = 200,
+  Fail = 400,
+  Crated = 201,
+}
 
 export class BoardController {
   static async createBoard(req: Request, res: Response) {
     const manager = getManager().getCustomRepository(BoardRepository);
-    await manager.createBoard(req, res);
+    try {
+      const { title } = req.body;
+      const board = new Board();
+      board.title = title;
+
+      const boardData = await manager.createBoard(board);
+
+      res.status(Status.Crated).json(boardData);
+    } catch (e) {
+      res.status(Status.Fail).json({
+        message: 'Something want wrong!!',
+      });
+    }
   }
 
   static async getAllBoards(req: Request, res: Response) {
     const manager = getManager().getCustomRepository(BoardRepository);
-    await manager.getAllBoards(req, res);
+    try {
+      const data = await manager.getAllBoards();
+
+      res.status(Status.Success).json(data);
+    } catch (e) {
+      res.status(Status.Fail).json({
+        message: 'Something want wrong!!',
+      });
+    }
   }
 
   static async getBoard(req: Request, res: Response) {
     const manager = getManager().getCustomRepository(BoardRepository);
-    await manager.getBoard(req, res);
+    try {
+      const { id } = req.params;
+      const oneData = await manager.getBoard(id);
+
+      res.status(Status.Success).json(oneData);
+    } catch (e) {
+      res.status(Status.Fail).json({
+        message: 'Something want wrong!!',
+      });
+    }
   }
 
   static async updateBoard(req: Request, res: Response) {
     const manager = getManager().getCustomRepository(BoardRepository);
-    await manager.updateBoard(req, res);
+    const { title, lists } = req.body;
+    const { id } = req.params;
+    try {
+      const updateData = await manager.updateBoard(title, lists, id);
+
+      res.status(Status.Success).json(updateData);
+    } catch (e) {
+      res.status(Status.Fail).json({
+        message: 'Something want wrong!!',
+      });
+    }
   }
 
   static async deleteBoard(req: Request, res: Response) {
     const manager = getManager().getCustomRepository(BoardRepository);
-    await manager.deleteBoard(req, res);
+    const { id } = req.params;
+    try {
+      await manager.deleteBoard(id);
+
+      res.status(Status.Success).json({
+        data: null,
+      });
+    } catch (e) {
+      res.status(Status.Fail).json({
+        message: 'Something want wrong!!',
+      });
+    }
   }
 }
--- src/services/board.ts
+++ src/services/board.ts
@@ -1,119 +1,44 @@
-import { Request, Response } from 'express';
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from '../entities/Board';
+
+enum Alias {
+  BoardTable = 'board',
+}
 
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
   // 1️⃣ get all boards
-  async getAllBoards(req: Request, res: Response) {
-    try {
-      const data = await this.createQueryBuilder('board').getMany();
-
-      res.status(200).json({
-        status: 'success',
-        results: data.length,
-        data: {
-          data,
-        },
-      });
-    } catch (e) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'Something want wrong!!',
-      });
-    }
+  getAllBoards() {
+    return this.createQueryBuilder(Alias.BoardTable).getMany();
   }
 
   // 2️⃣ get board
-  async getBoard(req: Request, res: Response) {
-    const { id } = req.params;
-    try {
-      const oneData = await this.createQueryBuilder('board')
-        .select()
-        .where('board.id = :query', { query: id })
-        .getOne();
-
-      res.status(200).json({
-        status: 'success',
-        data: {
-          oneData,
-        },
-      });
-    } catch (e) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'Something want wrong!!',
-      });
-    }
+  getBoard(boardId: string) {
+    return this.createQueryBuilder(Alias.BoardTable)
+      .select()
+      .where('board.id = :query', { query: boardId })
+      .getOne();
   }
 
   // 3️⃣ create new board
-  async createBoard(req: Request, res: Response) {
-    const { title } = req.body;
-    try {
-      const board = new Board();
-      board.title = title;
-
-      const boardData = await this.save(board);
-
-      res.status(201).json({
-        status: 'success',
-        data: boardData,
-      });
-    } catch (e) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'Something want wrong!!',
-      });
-    }
+  createBoard(newBoard: Board) {
+    return this.save(newBoard);
   }
 
   // 4️⃣ update board
-  async updateBoard(req: Request, res: Response) {
-    const { title, lists } = req.body;
-    const { id } = req.params;
-    try {
-      await this.createQueryBuilder('board')
-        .update(Board)
-        .set({ title, lists })
-        .where('board.id = :query', { query: id })
-        .execute();
-
-      const updatedData = await this.findOne(id);
-
-      res.status(200).json({
-        status: 'success',
-        data: {
-          updatedData,
-        },
-      });
-    } catch (e) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'Something want wrong!!',
-      });
-    }
+  updateBoard(title: string, lists: [], id: string) {
+    return this.createQueryBuilder(Alias.BoardTable)
+      .update(Board)
+      .set({ title, lists })
+      .where('board.id = :query', { query: id })
+      .execute();
   }
 
   // 5️⃣ delete board
-  async deleteBoard(req: Request, res: Response) {
-    const { id } = req.params;
-    try {
-      await this.createQueryBuilder('board')
-        .update(Board)
-        .set({ is_active: false })
-        .where('board.id = :query', { query: id })
-        .execute();
-
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    } catch (e) {
-      res.status(400).json({
-        status: 'fail',
-        message: 'Something want wrong!!',
-      });
-    }
+  deleteBoard(id: string) {
+    return this.createQueryBuilder(Alias.BoardTable)
+      .select()
+      .where('board.id = :query', { query: id })
+      .getOne();
   }
 }
