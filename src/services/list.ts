11d58ca51ff8822b61d83c6d715985d1e1db285a
Anush Markosyan <Anush_Markosyan@epam.com>
2021-11-04 11:45:20 +0400

    removed transaction from drag and drop

--- src/services/list.ts
+++ src/services/list.ts
@@ -1,9 +1,10 @@
-import { EntityRepository, Repository, getConnection } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { BoardEntity } from '../entities/Board';
 import { ListEntity } from '../entities/List';
 import { CardEntity } from '../entities/Card';
 import { ListInterface, IList } from '../interfaces';
 import { HttpErr } from '../exceptions/HttpError';
+import ExceptionMessages from '../exceptions/messages';
 
 interface newList {
   title: string;
@@ -57,33 +58,26 @@ export class ListRepository extends Repository<ListEntity> {
     listId: string,
     data: any[]
   ): Promise<ListInterface[]> {
-    const listIds = data.map(list => list.list_id);
-
-    const connection = getConnection();
-    const queryRunner = connection.createQueryRunner();
-
-    await queryRunner.startTransaction();
-
     try {
-      const listsArray = await queryRunner.manager.findByIds(
-        ListEntity,
-        listIds
-      );
+      const listIds = data.map(list => list.list_id);
+
+      const listsArray = await this.findByIds(listIds);
 
       for (const list of listsArray) {
         const listData = data.find(item => list.id === item.list_id);
 
         list.card_ids = listData.card_ids;
       }
+      await CardEntity.createQueryBuilder('card')
+        .update(CardEntity)
+        .set({ list_id: listId })
+        .where('card.id = :query', { query: cardId })
+        .execute();
+      await this.save(listsArray);
 
-      await queryRunner.manager.update(CardEntity, cardId, { list_id: listId });
-
-      await queryRunner.manager.save(ListEntity, listsArray);
-      await queryRunner.commitTransaction();
-      return await queryRunner.manager.findByIds(ListEntity, listIds);
+      return await this.findByIds(listIds);
     } catch {
-      await queryRunner.rollbackTransaction();
-      throw HttpErr.notFound('internal server error');
+      throw HttpErr.internalServerError(ExceptionMessages.INVALID.INPUT);
     }
   }
 
